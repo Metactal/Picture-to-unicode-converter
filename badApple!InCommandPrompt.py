032a6131f773c14ce7fd0ef8a64b527771a0c53d
Metactal <146528619+Metactal@users.noreply.github.com>
2023-10-29 20:09:19 +1100

    Update badApple!InCommandPrompt.py

--- badApple!InCommandPrompt.py
+++ badApple!InCommandPrompt.py
@@ -1,2 +1,2 @@
-import bichromaLibShadedEdition as blse
-blse.renderVideoInNotepad(570,30,r"badAppleFrames\【東方】Bad Apple!! ＰＶ【影絵】",5,"【東方】Bad Apple!! ＰＶ【影絵】.mp3")
+from bichromaLibShadedEdition import renderVideoInNotepad
+renderVideoInNotepad(570,30,r"badAppleFrames\【東方】Bad Apple!! ＰＶ【影絵】",5,"【東方】Bad Apple!! ＰＶ【影絵】.mp3")
